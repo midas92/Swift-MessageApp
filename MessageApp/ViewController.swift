dd0542e167de583c6f635dbf46682ded978ab34d
midas92 <dounanpar@gmail.com>
2022-03-14 13:03:16 -0400

    - Made iOS 15 new implementation work - Got most of the way with the old one, minor spacing issues

--- MessageApp/ViewController.swift
+++ MessageApp/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var sendButton: UIButton!
+    @IBOutlet private weak var newMessageTextView: UITextView!
+    @IBOutlet private weak var sendMessageContainerView: UIView!
+    
+    @IBOutlet weak var oldContainerViewBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if #available(iOS 15.0, *) {
+            applyiOS15KeyboardLayout()
+        } else {
+            applyOldiOSKeyboardLayout()
+        }
     }
 
 
+}
+
+// iOS 15 keyboard layout
+@available(iOS 15.0, *)
+extension ViewController {
+    private func applyiOS15KeyboardLayout() {
+        oldContainerViewBottomConstraint.isActive = false
+        sendMessageContainerView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor).isActive = true
+    }
+}
+
+// Older iOS keyboard layout
+extension ViewController {
+    private func applyOldiOSKeyboardLayout() {
+        oldContainerViewBottomConstraint.constant = view.safeAreaInsets.bottom
+        NotificationCenter.default.addObserver(self, selector: #selector(respondToKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc private func respondToKeyboard(notification: Notification) {
+        let notifInfo = notification.userInfo
+        if let endRect = notifInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            var offset = view.bounds.size.height - endRect.origin.y
+            if offset == 0.0 {
+                offset = view.safeAreaInsets.bottom
+            }
+            let duration = notifInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval ?? 0.5
+            UIView.animate(withDuration: duration) { [weak self] in
+                guard let welf = self else { return }
+                welf.oldContainerViewBottomConstraint.constant = offset
+                welf.view.layoutIfNeeded()
+            }
+        }
+    }
 }
 
